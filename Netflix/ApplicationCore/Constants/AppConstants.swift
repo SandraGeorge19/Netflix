736866f08c2732128742a63fe707a0a9e688c1b8
Sandra George <georgesandra592@gmail.com>
2023-07-16 17:55:35 +0300

    ...

--- Netflix/ApplicationCore/Constants/AppConstants.swift
+++ Netflix/ApplicationCore/Constants/AppConstants.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 struct AppConstants {
-    static let APIKey = "33b4943074f3779fc2a694b4f81b22da"
+    
 }
--- Netflix/ApplicationCore/Constants/AppKeys.swift
+++ Netflix/ApplicationCore/Constants/AppKeys.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+struct AppKeys {
+    static let APIKey = "33b4943074f3779fc2a694b4f81b22da"
+}
