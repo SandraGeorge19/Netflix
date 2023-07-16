de684424bc574773c4530afc91eba001d32d733b
Sandra George <georgesandra592@gmail.com>
2023-07-16 17:33:57 +0300

    add pods

---
+++ Netflix/ApplicationCore/Constants/AppConstants.swift
@@ -0,0 +1,12 @@
+//
+//  AppConstants.swift
+//  Netflix
+//
+//  Created by Sandra on 16/07/2023.
+//
+
+import Foundation
+
+struct AppConstants {
+    static let APIKey = "33b4943074f3779fc2a694b4f81b22da"
+}
---
+++ Netflix/ApplicationCore/NetworkManager/APIClient.swift
@@ -0,0 +1,16 @@
+//
+//  APIClient.swift
+//  Netflix
+//
+//  Created by Sandra on 16/07/2023.
+//
+
+import Foundation
+
+class APIClient {
+    static let shared = APIClient()
+    
+    private init() {
+        // private init
+    }
+}
